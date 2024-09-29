2e85db420cb71927425e3a2fdf9633de78fcaa1b
samifixx <46944032+samifixx@users.noreply.github.com>
2024-09-29 14:33:15 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Samifix'
+description 'samifix-mapcontrol Designed for survival or hard rp servers.'
+version '1.0.1'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts {
+    'client.lua',
+}
